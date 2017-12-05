c0315480c5d89f4bff37a9e9b523d1e5336e1acd
Adam Bergman <adam@fransvilhelm.com>
2017-12-05 01:30:49 +0100

    feat(environments): Setup environments

---
+++ src/module.test.js
@@ -0,0 +1,24 @@
+// import { CLIEngine } from 'eslint';
+
+// const cli = new CLIEngine({
+//   useEslintrc: false,
+//   baseConfig: {
+//     extends: ['./index.js'],
+//     env: {
+//       'src/extendscript': true,
+//     },
+//   },
+// });
+
+// const lint = code => {
+//   const linter = cli.executeOnText(code);
+//   return linter.results[0];
+// };
+
+test('Config test', () => {
+  // const result = lint(`
+  //   $.writeLn('Hello world!);
+  // `);
+
+  expect(true).toBe(true);
+});
